61b1aa0ee6571158395a94db29ac7c0c99445b27
Anshdeep Chawla <anshchawla94@gmail.com>
2025-07-10 16:54:33 +0530

    Scffolding

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  protected title = 'BotFusion';
-}
+export class AppComponent {}
